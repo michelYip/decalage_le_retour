0418290d5873e3eb71ad54f33cfa194f6a9465a1
michelYip <wu.michel.yip@gail.com>
2019-01-17 14:50:33 +0100

    Start GrabSeagullScript + Correcting Player movement

---
+++ scripts/controllers/GrabSeagull.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GrabSeagull : MonoBehaviour
+{
+    public SteamVR_TrackedObject trackedObject;
+    public SteamVR_Controller.Device device;
+    public float grabDistance = 10.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        device = SteamVR_Controller.Input((int)trackedObject.index);
+        Debug.Log(this.transform.root.gameObject);
+        if (device.GetPress(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            if (Physics.Raycast(transform.position, transform.up, out hit, grabDistance)){
+                Debug.DrawRay(transform.position, transform.up * grabDistance, Color.red);
+                if(hit.collider.gameObject.tag == "seagull")
+                {
+                    this.transform.root.gameObject.transform.SetParent(hit.collider.gameObject.transform);
+                }
+            }
+        }
+    }
+}
+  
