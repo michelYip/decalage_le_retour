5145ccb02727d43feade246546a4a7dd2d80e867
elessach <elessach@gmail.com>
2019-01-18 15:27:55 +0100

    gravity and controllers

---
+++ scripts/physic/GravityBody.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Rigidbody))]
+public class GravityBody : MonoBehaviour
+{
+    public GravityAttractor attractor;
+    private Transform myTransform;
+
+    void Start()
+    {
+        GetComponent<Rigidbody>().useGravity = false;
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+
+        myTransform = transform;
+    }
+
+    void FixedUpdate()
+    {
+        if (attractor) attractor.Attract(myTransform);
+    }
+}
