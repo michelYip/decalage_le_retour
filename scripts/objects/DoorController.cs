875054a0cc3630eb1ce9342a3fc680ae170cf660
Guillaume DUT <GuillaumeDUT@github.com>
2019-01-18 13:09:13 +0100

    door animation ok

--- scripts/objects/DoorController.cs
+++ scripts/objects/DoorController.cs
@@ -9,16 +9,34 @@ public class DoorController : MonoBehaviour {
 	
 	void Start () {
 		animator = GetComponent<Animator>();
+
+
 	}
 	
-	void Update () {
+    void Awake()
+    {
+        var sphereCollider = gameObject.AddComponent<SphereCollider>();
+        sphereCollider.isTrigger = true;
+    }
+
+	void Update () {    
 	}
 
-	void OnCollisionEnter(Collision col) {
+    void OnCollisionEnter(Collision col) {
         if (col.gameObject.CompareTag("User"))
         {
             animator.SetTrigger("Enter");
-			SceneManager.LoadScene(this.gameObject.tag);
+            SceneManager.LoadScene(this.gameObject.tag);
         }
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        //Debug.Log("In");
+        animator.SetBool("isNear", true);
+    }
+    void OnTriggerExit(Collider col)
+    {
+        //Debug.Log("Out");
+        animator.SetBool("isNear", false);
     }
 }
