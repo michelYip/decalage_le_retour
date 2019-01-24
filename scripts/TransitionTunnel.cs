95640e9764c0de5b755ec29addbadccd030473ea
elessach <elessach@gmail.com>
2019-01-24 15:36:48 +0100

    controllers repaired in all world | transition tunnel | gravity in organicube

---
+++ scripts/TransitionTunnel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionTunnel : MonoBehaviour
+{
+    GameObject user;
+    string destination;
+
+    void Start()
+    {
+        user = GameObject.FindWithTag("User");
+        destination = Attributes.Destination;
+    }
+    
+    void Update()
+    {
+        if(user.transform.position.y < -300)
+        {
+            SceneManager.LoadScene(destination);
+        }
+    }
+}
